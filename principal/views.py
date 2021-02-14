3b620fa0e5c6fe64748620e0c33e3eddc3df65ab
Joel Yanotka <joelyanotka@gmail.com>
2021-02-14 10:00:53 -0300

    Definidas vistas simples para el registro y login

--- principal/views.py
+++ principal/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
+#from django.template import loader
 from django.shortcuts import render
 
-# Create your views here.
+
+def log_in(request):
+    #template = loader.get_template('principal/login.html')
+    return render(request, 'principal/login.html', context=None)
+
+def sign_up(request):
+    #template = loader.get_template('principal/signup.html')
+    return render(request, 'principal/signup.html', context=None)
