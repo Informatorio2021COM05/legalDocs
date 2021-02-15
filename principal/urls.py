3763f315fa50b5c0bd215aa8c1baed736183104c
Miqueas <miqueas.yanotka@gmail.com>
2021-02-15 18:32:31 -0300

    modificación de urls y views

--- principal/urls.py
+++ principal/urls.py
@@ -6,4 +6,12 @@ app_name = 'principal'
 urlpatterns = [
     path('login/', views.log_in, name='login'),
     path('signup/', views.sign_up, name='signup'),
+    path('validar_documento/', views.validar_documento, name='validar_documento'),
+    path('cargar_documento/', views.cargar_documento, name='cargar_documento'),
+    path('buscar_escribano/', views.buscar_escribano, name='buscar_escribano'),
+    path('perfil_cliente/', views.perfil_cliente, name='perfil_cliente'),
+    path('perfil_escribano_desde_usuario/', views.perfil_escribano_desde_usuario, name='perfil_escribano_desde_usuario'),
+    path('perfil_escribano/', views.perfil_escribano, name='perfil_escribano'),
+    path('resultado_busqueda/', views.resultado_busqueda, name='resultado_busqueda'),
+
 ]
--- principal/views.py
+++ principal/views.py
@@ -10,3 +10,24 @@ def log_in(request):
 def sign_up(request):
     #template = loader.get_template('principal/signup.html')
     return render(request, 'principal/signup.html', context=None)
+
+def validar_documento(request):
+    return render(request, 'principal/validar_documento.html', context=None)
+
+def cargar_documento(request):
+    return render(request, 'principal/cargar_documento.html', context=None)
+
+def buscador_escribano(request):
+    return render(request, 'principal/buscador_escribano.html', context=None)
+
+def perfil_cliente(request):
+    return render(request, 'principal/perfil_cliente.html', context=None)
+
+def perfil_escribano_desde_cliente(request):
+    return render(request, 'principal/perfil_escribano_desde_cliente.html', context=None)
+
+def perfil_escribano(request):
+    return render(request, 'principal/perfil_escribano.html', context=None)
+
+def resultado_busqueda(request):
+    return render(request, 'principal/resultado_busqueda.html', context=None)
