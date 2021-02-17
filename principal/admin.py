23c6efd25b89d985e3d4020797915c77b71711a0
Miqueas <miqueas.yanotka@gmail.com>
2021-02-17 17:17:18 -0300

    se agregó los modelos al admin

--- principal/admin.py
+++ principal/admin.py
@@ -1,3 +1,11 @@
+from principal.models import *
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Usuarios)
+admin.site.register(Escribano)
+admin.site.register(Cliente)
+admin.site.register(Turno)
+admin.site.register(Documento)
+
