7c7db37b1a72eb37b31fb35b2f0b1cff34a56bc3
Joel Yanotka <joelyanotka@gmail.com>
2021-02-19 12:08:35 -0300

    Creado forms.py con clases para registro

---
+++ principal/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core import validators
+
+
+class BasicSignupForm(forms.Form):
+    usuario = forms.CharField(validators=[
+        validators.MaxLengthValidator(30)])
+    nombre = forms.CharField(validators=[
+        validators.MaxLengthValidator(30)])
+    apellido = forms.CharField(validators=[
+        validators.MaxLengthValidator(30)])
+    dni = forms.IntegerField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(100000000)])
+    email = forms.EmailField()
+    contraseña = forms.CharField(max_length=30, widget=forms.PasswordInput)
+    confirmar_contraseña = forms.CharField(max_length=30, widget=forms.PasswordInput)
+
+
+class ExtendSignupForm(BasicSignupForm):
+    matricula = forms.IntegerField()
+    provincia = forms.CharField(validators=[
+        validators.MaxLengthValidator(50)])
+    ciudad = forms.CharField(validators=[
+        validators.MaxLengthValidator(50)])
+    calle = forms.CharField(validators=[
+        validators.MaxLengthValidator(50)])
+    altura = forms.IntegerField()
+    piso = forms.IntegerField(validators=[
+        validators.MinValueValidator(0)])
+    numeroPuerta = forms.CharField(validators=[
+        validators.MaxLengthValidator(5)])
+
+
+
+from django.forms import ModelForm, fields
+from .models import Cliente, Escribano
+
+
+class ClienteForm(ModelForm):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+
+class EscribanoForm(ModelForm):
+    class Meta:
+        model = Escribano
+        fields = '__all__'
