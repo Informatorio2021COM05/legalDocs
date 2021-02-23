3aef2ab58b942a005f9f74284f2f24d6b16e8f00
Joel Yanotka <joelyanotka@gmail.com>
2021-02-23 11:40:37 -0300

    Eliminado el usuario personalizado para el admin

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -1,16 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser, Escribano
+from .models import *
 
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['email', 'username', 'is_staff', ]
-
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
 admin.site.register(Escribano)
