59446b49f6b4b5968bd49699d578ecd7fb5f740f
Joel Yanotka <joelyanotka@gmail.com>
2021-02-24 14:17:43 -0300

    Añadida la opción de provincias en el registro de escribano

--- cuentas/forms.py
+++ cuentas/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.db.models.query_utils import Q
 from .models import CustomUser, Escribano
 from django import forms
 
@@ -17,8 +18,16 @@ class CustomUserChangeForm(UserChangeForm):
 
 
 class EscribanoCreationForm(UserCreationForm):
+    opciones = ((1, 'Buenos Aires'), (2, 'Capital Federal'), (3, 'Catamarca'), (4, 'Chaco'),
+        (5, 'Chubut'), (6, 'Córdoba'), (7, 'Corrientes'), (8, 'Entre Ríos'), (9, 'Formosa'),
+        (10, 'Jujuy'), (11, 'La Pampa'), (12, 'La Rioja'), (13, 'Mendoza'), (14, 'Misiones'),
+        (15, 'Neuquén'), (16, 'Río Negro'), (17, 'Salta'), (18, 'San Juan'), (19, 'San Luis'),
+        (20, 'Santa Cruz'), (21, 'Santa Fe'), (22, 'Santiago del Estero'),
+        (23, 'Tierra del Fuego'), (24, 'Tucumán'),
+        )
+    
     matricula = forms.IntegerField()
-    provincia = forms.CharField(max_length=50)
+    provincia = forms.ChoiceField(choices=opciones)
     ciudad = forms.CharField(max_length=50)
     calle = forms.CharField(max_length=50)
     altura = forms.IntegerField()
