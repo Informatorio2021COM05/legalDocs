97a50d3528f3ddbf817bed4122b02098d6dee420
Joel Yanotka <joelyanotka@gmail.com>
2021-02-25 14:00:34 -0300

    Eliminación de campos redundantes en las vista de creación de documento

--- principal/forms.py
+++ principal/forms.py
@@ -2,18 +2,14 @@ from django import forms
 from django.forms import ModelForm, DateInput
 from django.forms.widgets import TimeInput
 from .models import Documento, Turno
-from .validators import validate_file_extension
 
 
 class DocumentoForm(ModelForm):
-    título = forms.CharField(max_length=50)
-    descripción = forms.CharField(max_length=300, required=False)
-    número_de_páginas = forms.IntegerField()
-    archivo = forms.FileField(required=False, validators=[validate_file_extension])
+    
 
     class Meta:
         model = Documento
-        fields = ('titulo', 'descripcion', 'paginas', 'archivo')
+        fields = ('cliente', 'titulo', 'descripcion', 'paginas', 'archivo')
 
 
 
